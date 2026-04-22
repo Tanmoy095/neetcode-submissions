a52b1ae2235ca9f10a1957ae38530b81f3e2e8b1
Aunmoy Dey <90642341+Tanmoy095@users.noreply.github.com>
2026-04-23 02:27:44 +0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+
+	current := head 
+
+	for current!=nil {
+
+		tempNext := current.Next
+		current.Next = prev
+		prev =  current 
+		current = tempNext		 
+	}
+	return prev
+    
+}
