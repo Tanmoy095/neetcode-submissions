9ff94a903aaf1c1eb6056425ebbad4262505cbb9
Aunmoy Dey <adtanmoy95@gmail.com>
2026-04-30 19:12:50 +0600

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func maxDepth(root *TreeNode) int {
+	//Base Case 
+
+	if root == nil{
+		return 0;
+	}
+
+	leftDepth := maxDepth(root.Left);
+	rightDepth := maxDepth(root.Right);
+
+	return max(leftDepth,rightDepth)+1
+
+
+    
+}
